1897706fb61b4dff029f902454601176ff1a4be0
Sujit Thapa <megasuziet@gmail.com>
2024-01-29 15:10:35 +0100

    Updated producer.py

--- producer.py
+++ producer.py
@@ -9,7 +9,7 @@ topic = 'test'
 # Initialize Kafka producer with bootstrap servers and a JSON value serializer
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
-# Produce 11 messages to the Kafka topic with a 'number' field in the message data
+# Produce 11 messages to the Kafka topic with 'number' field in the message data
 for i in range(11):
     data = {'number': i} 
     producer.send(topic, value=data)

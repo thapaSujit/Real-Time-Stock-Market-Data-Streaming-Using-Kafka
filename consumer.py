3bb6ba11ac222a46976caef7c5edcc339b567873
Sujit Thapa <megasuziet@gmail.com>
2024-01-29 15:09:54 +0100

    Updated consumer.py

--- consumer.py
+++ consumer.py
@@ -5,8 +5,7 @@ import json
 # Set the Kafka topic
 topic = 'test'
 
-# Initialize Kafka consumer with bootstrap servers, auto offset reset to 'earliest',
-# and a JSON value deserializer
+# Initialize Kafka consumer with bootstrap servers, auto offset reset to 'earliest', and a JSON value deserializer
 consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'],
                          auto_offset_reset='earliest',
                          value_deserializer=lambda x: json.loads(x.decode('utf-8')))
